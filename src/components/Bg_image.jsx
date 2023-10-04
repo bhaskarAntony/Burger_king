f1b312703df2f29459597b9764db5df17cedfa68
bhaskarAntonyCoder <codercss3958@gmail.com>
2023-10-05 02:18:13 +0530

    all burger king files

--- src/components/Bg_image.jsx
+++ src/components/Bg_image.jsx
@@ -3,8 +3,8 @@ import '../styles/bg-image.css'
 
 function Bg_image() {
   return (
-    <section className='bg-image mt-5'>
-      <div className="bg-image-text">
+    <section className='bg-image mt-5 text-center'>
+      <div className="bg-image-text p-5 ">
       <h1 className='text-white hero-heading mb-3'>WE SERVE YOU BEST</h1>
       <div className="devider mb-3"></div>
       <p className='p-small text-white text-center'>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Placeat, blanditiis.</p>
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,11 +3,11 @@ import '../styles/subscribe.css'
 
 function Subscribe() {
   return (
-    <section className='subscribe container mt-5 rounded-4'>
+    <section className='subscribe container mt-5 rounded-4 p-5'>
     <div className="container">
         <div className="row">
             <div className="col-12 col-md-12 col-lg-5">
-                <div className="subscribe-text">
+                <div className="subscribe-text text-center">
                     <h2 className="text-black fw-bold">GET EXCLUSIVE UPDATE</h2>
                     <p className="p-small text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
@@ -16,10 +16,10 @@ function Subscribe() {
                 <div className="subscribe-form">
                     <div className="row">
                         <div className="col-12 col-md-6 col-lg-8">
-                            <input type="text" className="form-control border-0 bg-tranparent fs-5 p-3" placeholder='Email'/>
+                            <input type="text" className="form-control border-0 bg-tranparent fs-5 p-3 mb-2" placeholder='Email'/>
                         </div>
                         <div className="col-12 col-md-6 col-lg-4 d-flex align-items-center">
-                            <button className="btn bg-black text-white rounded-2 p-3 px-4"><i class="bi bi-send-fill mx-1"></i>Subscribe Now</button>
+                            <button className="btn bg-black text-white rounded-2 p-3 px-4 mb-2 hero-btn"><i class="bi bi-send-fill mx-1"></i>Subscribe Now</button>
                         </div>
                     </div>
                 </div>
